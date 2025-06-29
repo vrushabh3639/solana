d59bc2fddd3d9ac7802f192e4debea135140cc3d
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-06-29 19:29:52 +0530

    i should use keypair path i guess rather then pasting json array

--- basics/dist/01basics.js
+++ basics/dist/01basics.js
@@ -7,7 +7,7 @@
 // getting test sol using airdrop
 import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
 const connection = new Connection("https://devnet.helius-rpc.com/?api-key=a1ff3fd7-1ab4-44aa-ad77-8a8a7c0a09b2", "confirmed");
-const secret = Uint8Array.from([ /* I leave my pvt key here and pushed this to github lol */]);
+const secret = Uint8Array.from([ /* i leave my pvt key here and pushed this to github lol */]);
 const wallet = Keypair.fromSecretKey(secret);
 const signature = await connection.requestAirdrop(wallet.publicKey, LAMPORTS_PER_SOL);
 const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
