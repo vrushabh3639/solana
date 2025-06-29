fb1356b273af8489528ad0d6ce72e9fcae34339a
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-06-29 19:57:58 +0530

    keypair from file

---
+++ basics/dist/02basics.js
@@ -0,0 +1,7 @@
+import fs from "fs";
+import { Keypair } from "@solana/web3.js";
+const keypairPath = 'D:/keypair.json';
+const data = await fs.promises.readFile(keypairPath, 'utf8');
+const secretKey = Uint8Array.from(JSON.parse(data));
+const keypair = Keypair.fromSecretKey(secretKey);
+console.log("Public Key:", keypair.publicKey.toBase58());
