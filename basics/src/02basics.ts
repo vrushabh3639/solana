bc6c69123f17386a79c60dce83c289a52842b591
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-06-30 14:22:53 +0530

    send sol

--- basics/src/02basics.ts
+++ basics/src/02basics.ts
@@ -1,3 +1,5 @@
+// keypair from a file
+
 import fs from "fs";
 import { Keypair } from "@solana/web3.js";
 
@@ -7,4 +9,22 @@ const data = await fs.promises.readFile(keypairPath, 'utf8');
 const secretKey = Uint8Array.from(JSON.parse(data));
 const keypair = Keypair.fromSecretKey(secretKey);
 
-console.log("Public Key:", keypair.publicKey.toBase58());
+// console.log("Public Key:", keypair.publicKey.toBase58());
+
+
+// verify a keypair
+
+// import { Keypair, PublicKey } from "@solana/web3.js";
+
+const publicKey = new PublicKey("PremxWSjbuD1S7cVwnWpWGE3byxYRuP55rfKvtR17i3")
+// keypair is declared above
+// console.log(keypair.publicKey.toBase58() === publicKey.toBase58());
+
+
+// validate a public key
+
+import { PublicKey } from "@solana/web3.js";
+
+// on curve address
+const key = new PublicKey("PremxWSjbuD1S7cVwnWpWGE3byxYRuP55rfKvtR17i3")
+console.log(`isOnCurve: ${PublicKey.isOnCurve(key.toBytes())}`);
---
+++ transactions/dist/sendsol.js
@@ -0,0 +1,22 @@
+// send sol
+import { Connection, Keypair, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import fs from "fs";
+const connection = new Connection("https://devnet.helius-rpc.com/?api-key=a1ff3fd7-1ab4-44aa-ad77-8a8a7c0a09b2", "confirmed");
+const keypairPath = 'D:/keypair.json';
+const data = await fs.promises.readFile(keypairPath, 'utf8');
+const secretKey = Uint8Array.from(JSON.parse(data));
+const fromKeypair = Keypair.fromSecretKey(secretKey);
+const toKeypair = Keypair.generate();
+// const airdropSignature = await connection.requestAirdrop(
+//     fromKeypair.publicKey,
+//     LAMPORTS_PER_SOL
+// );
+// await connection.confirmTransaction(airdropSignature)
+const lamportsToSend = 1000000;
+const transferTransaction = new Transaction().add(SystemProgram.transfer({
+    fromPubkey: fromKeypair.publicKey,
+    toPubkey: toKeypair.publicKey,
+    lamports: lamportsToSend
+}));
+const signature = await sendAndConfirmTransaction(connection, transferTransaction, [fromKeypair]);
+console.log("Transaction Signature:", signature);
