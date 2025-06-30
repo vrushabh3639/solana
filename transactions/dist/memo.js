08e1b89b290cc8437cf76cd9ef6e1c84f1903035
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-06-30 16:21:37 +0530

    memo

---
+++ transactions/dist/memo.js
@@ -0,0 +1,16 @@
+// add memo to a transaction
+import { Connection, Keypair, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
+import { createMemoInstruction } from "@solana/spl-memo";
+import fs from "fs";
+const connection = new Connection("https://devnet.helius-rpc.com/?api-key=a1ff3fd7-1ab4-44aa-ad77-8a8a7c0a09b2", "confirmed");
+const keypairPath = 'D:/keypair.json';
+const data = await fs.promises.readFile(keypairPath, 'utf8');
+const secretKey = Uint8Array.from(JSON.parse(data));
+const feePayer = Keypair.fromSecretKey(secretKey);
+// create a memo instruction
+const memoInstruction = createMemoInstruction("Hello, world!");
+// create transaction and add the memo instruction
+const transaction = new Transaction().add(memoInstruction);
+// sign and send transaction
+const transactionSignature = await sendAndConfirmTransaction(connection, transaction, [feePayer]);
+console.log("Transaction Signature: ", transactionSignature);
