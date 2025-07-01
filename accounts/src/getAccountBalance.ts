ebd9a0786e3e285feb45f98e1b956ae1a2a38d5e
Vrushabh <vrushabhkakadiya3639@gmail.com>
2025-07-01 11:14:33 +0530

    more about accounts

---
+++ accounts/src/getAccountBalance.ts
@@ -0,0 +1,11 @@
+import {
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  LAMPORTS_PER_SOL
+} from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("mainnet-beta"), "confirmed");
+const address = new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
+const balance = await connection.getBalance(address);
+console.log(`Balance: ${balance / LAMPORTS_PER_SOL} SOL`);
